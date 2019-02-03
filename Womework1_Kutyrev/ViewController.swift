00f9a8904b696b63464b5285582ed7aee48ee7e5
Iurikman <y.m.kutyrev@gmail.com>
2019-02-03 19:55:40 +0300

    les 2

---
+++ Womework1_Kutyrev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Womework1_Kutyrev
+//
+//  Created by Yuriy Kutyrev on 26.01.2019.
+//  Copyright © 2019 Yuriy Kutyrev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
